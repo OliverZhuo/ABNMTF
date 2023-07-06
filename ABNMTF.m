596de3b2de7674354ea1d30788b6c07b4e9a4725
ZhiJian Zhuo <634988568@qq.com>
2023-07-06 23:25:29 +0800

    create ABNMTF
    create ABNMTF

---
+++ ABNMTF.m
@@ -0,0 +1,32 @@
+function [U,W,out] = ABNMTF(A, k)
+epsilon = sqrt(eps);
+
+n = size(A,1);
+U = rand(n,k);
+W = eye(k,k);
+O = eye(k);
+B = eye(k);
+
+a = 1;
+b = 100;
+c = 1;
+d = 100;
+nu = 100;
+
+n_iter = 500;
+
+
+for iter = 1:n_iter
+    U = U .* ((A'+A)*U*(W+W')) ./ (2.*(U*W'*U'*U*W'+U*W*U'*U*W+U*O ) + epsilon);
+    W = W .* (U'*A*U+U'*A'*U+2*nu.*eye(k,k) ) ./ (2.*U'*U*W*U'*U+W*B+2*nu.*W + epsilon);
+
+    alpha = (n + a - 1) ./ (sum(U.*U,1)+b);
+    beta = (0.5*n + c - 1) ./ (0.5*sum(W.*W,1)+d);
+    for i=1:k
+        O(i,i) = alpha(i);
+        B(i,i) = beta(i);
+    end
+
+end
+
+end
